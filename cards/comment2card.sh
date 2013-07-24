42bc4352c2075c25787c24b99797a09eef5e5ca9
Patrick Drotleff <patrick.d@ventral.org>
2013-07-24 10:26:23 +0200

    Modified comment2card script to take a full issue url with comment anchor.

--- cards/comment2card.sh
+++ cards/comment2card.sh
@@ -2,21 +2,28 @@
 if [ $# -eq 0 ]
   then
     echo ""
-    echo "Usage: $0 [issue url] [comment id]"
+    echo "Usage: $0 [issue url]#comment-[comment id]"
     echo ""
-    echo "Example comment: https://drupal.org/node/1170192#comment-6578792"
-    echo " => $0 https://drupal.org/node/1170192 6578792"
+    echo "Example comment: "
+    echo " => $0 https://drupal.org/node/1170192#comment-6578792"
+    echo ""
+    echo "Easiest way to get this URL is by clicking on the comment anchor."
     echo ""
     exit
 fi
 
+IFS='#' read -ra ARG <<< "$1"
+
+echo "ISSUE URL:  ${ARG[0]}"
+echo "COMMENT ID: ${ARG[1]}"
+
 echo "DOWNLOADING ISSUE PAGE"
 
-curl "$1" > tmp.htm
+curl "${ARG[0]}" > tmp.htm
 
 echo "SELECTING COMMENT"
 sed '/<!DOCTYPE html>/,/<body/ !d' tmp.htm > comment.htm
-sed "/<a id=\"comment-$2\">/,/<a id=\"comment-/ !d" tmp.htm >> comment.htm
+sed "/<a id=\"${ARG[1]}\">/,/<a id=\"comment-/ !d" tmp.htm >> comment.htm
 echo '</body></html>' >> comment.htm
 sed '/comment_forbidden/d' ./comment.htm > comment2.htm
 
